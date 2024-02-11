9d1c062db0dc394fbeb6c8b7c7e487d529ea422a
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-02-11 19:36:26 +0000

    improve environment variable error messages

--- apps/bot/src/env.ts
+++ apps/bot/src/env.ts
@@ -1,28 +1,29 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-    DISCORD_TOKEN: z
-        .string()
-        .min(1)
-        .describe('The token of the bot. You can find this in the Discord Developer Portal.'),
-    NODE_ENV: z
-        .enum(['development', 'production'])
-        .describe(
-            'The environment the bot is running in. This should be set to "production" in production or "development" in development.'
-        ),
-    CLIENT_ID: z
-        .string()
-        .min(1)
-        .describe('The ID of the bot. You can find this in the Discord Developer Portal.'),
-    GUILD_ID: z
-        .string()
-        .min(1)
-        .optional()
-        .describe(
-            'The ID of the guild to register commands in. This is only required in production.'
-        )
+    DISCORD_TOKEN: z.string().min(1, 'Discord token must be defined to authenticate the bot.'),
+    NODE_ENV: z.enum(['development', 'production']),
+    CLIENT_ID: z.string().min(1, 'Client ID must be defined to register slash commands.'),
+    GUILD_ID: z.string().optional()
 });
 
 export type Env = z.infer<typeof envSchema>;
 
-export const env = envSchema.parse(process.env);
+export const env = await envSchema.safeParseAsync(process.env).then((result) => {
+    if (!result.success) {
+        console.error(
+            result.error.errors
+                .map((error) => `Invalid ${error.path} in environment. ${error.message}`)
+                .join('\n')
+        );
+
+        process.exit(1);
+    }
+
+    // I could loop through the keys, but that will risk exposing sensitive information like tokens.
+    console.info('NODE_ENV =', result.data.NODE_ENV);
+    console.info('CLIENT_ID =', result.data.CLIENT_ID);
+    console.info('GUILD_ID =', result.data.GUILD_ID);
+
+    return result.data;
+});
