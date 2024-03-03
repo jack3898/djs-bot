7e5e87d33a1ab3e90b136fe394c4941d90c709ff
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-03-03 18:05:16 +0000

    #12 add tailwindcss

--- apps/client/src/pages/JobStatus.tsx
+++ apps/client/src/pages/JobStatus.tsx
@@ -3,5 +3,5 @@ import { useParams } from 'react-router-dom';
 export function JobStatus(): JSX.Element {
     const { id } = useParams();
 
-    return <div>Job Status {id}</div>;
+    return <div className="bg-black text-white block">Job Status {id}</div>;
 }
